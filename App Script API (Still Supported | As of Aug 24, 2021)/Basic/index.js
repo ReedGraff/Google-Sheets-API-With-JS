e3b2111436ba201413671e26b67d103450d1cd2f
Reed Graff <71910763+ReedGraff@users.noreply.github.com>
2021-08-24 16:24:42 -0500

    Update index.js

--- App Script API (Still Supported | As of Aug 24, 2021)/Basic/index.js
+++ App Script API (Still Supported | As of Aug 24, 2021)/Basic/index.js
@@ -21,7 +21,7 @@ function start() {
     setTimeout(function() {
         if (try_num >= 1) {
             try_num -= 1
-            fetch_data('https://script.google.com/macros/s/AKfycbx_hqjJOoJMfukVKgatlPNsC1K38KYcN2PFT1eVzB4fuQMfMzcpHW38rokrv9g_GdEV/exec')
+            fetch_data('SCRIPT API URL')
         }
     }, delayInMilliseconds)
 }
@@ -29,12 +29,9 @@ function start() {
 function main(data) {
     console.log(data)
     var rows = data[0].data
-    console.log(rows)
     for (var row_num in rows) {
         document.getElementById("table").innerHTML += "<tr id='" + row_num + "'></tr>"
-        console.log(rows[row_num])
         for (var element_num in rows[row_num]) {
-            console.log(rows[row_num][element_num])
             document.getElementById(row_num).innerHTML += "<th>" + rows[row_num][element_num] + "</th>"
         }
     }
