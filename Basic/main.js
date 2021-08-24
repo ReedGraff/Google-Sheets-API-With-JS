f462b0eb3969d4eff896bc173f472fc97f86af0c
Reed Graff <71910763+ReedGraff@users.noreply.github.com>
2021-08-24 15:19:42 -0500

    Delete Basic directory

--- Basic/main.js
+++
@@ -1,8 +0,0 @@
-fetch("ENDPOINT")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-
-        // SET VALUES OF INNER TEXT
-        document.querySelector("#output").innerHTML = data.PROPERTYPATH
-    })
